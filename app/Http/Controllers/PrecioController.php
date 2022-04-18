1479869ac6c11d0d65388e4a09b4be28f2891614
Nicolas Colque <nicolascon93@gmail.com>
2022-04-18 18:21:30 -0400

    Creación de los archivos model, factory, migration, seeder, request, controller y policy para Empresa, Precio, Alumno y Pago

---
+++ app/Http/Controllers/PrecioController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Precio;
+use App\Http\Requests\StorePrecioRequest;
+use App\Http\Requests\UpdatePrecioRequest;
+
+class PrecioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePrecioRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePrecioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Precio  $precio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Precio $precio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Precio  $precio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Precio $precio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePrecioRequest  $request
+     * @param  \App\Models\Precio  $precio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePrecioRequest $request, Precio $precio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Precio  $precio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Precio $precio)
+    {
+        //
+    }
+}
